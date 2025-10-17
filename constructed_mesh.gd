20c5293875b07da93b7f2f27525c0f5554698b05
DevinBayly <baylyd@arizona.edu>
2025-10-17 09:56:57 -0700

    stuck on not seeing the video play

--- constructed_mesh.gd
+++ constructed_mesh.gd
@@ -1,6 +1,13 @@
 extends Node3D
 @onready var fps = $Character
+var vid_scene = preload("res://video.tscn")
 var anchors =[]
+var vmat
+
+func _ready() -> void:
+	vmat = StandardMaterial3D.new()
+	vmat.resource_local_to_scene=true
+	
 func place_object(coord):
 	var cube = MeshInstance3D.new()
 	cube.mesh = BoxMesh.new()
@@ -11,13 +18,25 @@ func place_object(coord):
 	anchors.push_back(cube)
 	if anchors.size()==3:
 		make_plane()
+		add_viewport()
+
+func add_viewport():
+	var viewport = SubViewport.new()
+	add_child(viewport)
+	var vid_instance = vid_scene.instantiate()
+	viewport.add_child(vid_instance)
+	# now we have to add things to the vmat
+	vmat.albedo_texture = viewport.get_texture()
+		
+	
+	
 
 func make_plane():
 	# use the immediate mesh option
 	var custom_plane = MeshInstance3D.new()
-	var mat = StandardMaterial3D.new()
+	
 	var mesh = ImmediateMesh.new()
-	mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLES,mat)
+	mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLES,vmat)
 	for a in anchors:
 		mesh.surface_add_vertex(a.position)
 	# use the first and third plus a new point to make the second triangle for the plane
@@ -32,6 +51,8 @@ func make_plane():
 	mesh.surface_end()
 	custom_plane.mesh = mesh
 	add_child(custom_plane)
+	mesh.resource_local_to_scene = true
+	
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
---
+++ video_stream_player.gd
@@ -0,0 +1,6 @@
+extends VideoStreamPlayer
+
+
+func _on_finished() -> void:
+	print("video done")
+	pass # Replace with function body.
