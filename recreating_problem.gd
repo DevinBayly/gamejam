179345120db9c052cad1661eee02a08b884afc52
DevinBayly <baylyd@arizona.edu>
2025-10-20 12:22:53 -0700

    ok got it to work

--- recreating_problem.gd
+++ recreating_problem.gd
@@ -9,16 +9,7 @@ func _ready() -> void:
 	var vid_instance = vid_scene.instantiate()
 	viewport.add_child(vid_instance)
 	var plane  = MeshInstance3D.new()
-	plane.mesh = QuadMesh.new()
 	
-	var vmat = StandardMaterial3D.new()
-	vmat.albedo_texture = viewport.get_texture()
-	plane.set_surface_override_material(0,vmat)
-	plane.rotate_y(180)
-	add_child(plane)
-func _old() -> void:
-	var vmat = StandardMaterial3D.new()
-	var custom_plane = MeshInstance3D.new()
 	var vertices = PackedVector3Array()
 	
 	# maek a collection of vertices that are ok for a plane
@@ -29,17 +20,33 @@ func _old() -> void:
 	vertices.push_back(Vector3(1,0,0))
 	vertices.push_back(Vector3(1,1,0))
 	vertices.push_back(Vector3(0,1,0))
-
+	var uvs = PackedVector2Array()
+	uvs.push_back(Vector2(0,0))
+	uvs.push_back(Vector2(1,0))
+	uvs.push_back(Vector2(0,1))
+	
+	uvs.push_back(Vector2(1,0))
+	uvs.push_back(Vector2(1,1))
+	uvs.push_back(Vector2(0,1))
 	# Initialize the ArrayMesh.
 	var arr_mesh = ArrayMesh.new()
 	var arrays = []
 	arrays.resize(Mesh.ARRAY_MAX)
 	arrays[Mesh.ARRAY_VERTEX] = vertices
+	arrays[Mesh.ARRAY_TEX_UV] = uvs
 
 # Create the Mesh.
 	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
-	arr_mesh.resource_local_to_scene=true
-	custom_plane.mesh = arr_mesh
+	plane.mesh = arr_mesh
+	var vmat = StandardMaterial3D.new()
+	vmat.albedo_texture = viewport.get_texture()
+	plane.set_surface_override_material(0,vmat)
+	plane.rotate_y(180)
+	add_child(plane)
+func _old() -> void:
+	var vmat = StandardMaterial3D.new()
+	var custom_plane = MeshInstance3D.new()
+	
 	add_child(custom_plane)
 	
 	vmat.resource_local_to_scene = true
